ba672d4b1bd6a8cb1264b58fb6cc6004dca1227f
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-28 09:57:53 -0700

    redo the resume link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
   return (<Container>
         <div className="card">
           <header>Word document of Resume</header>
-          <a href="https://drive.google.com/file/d/1NbmU6QfW7C9HB6-J7fmK4Po0hTWvjXj_/view?usp=sharing">  
+          <a href="https://drive.google.com/file/d/17t-qFaOgAtLic_iNeH7SWMJi_Npjlt-7/view?usp=sharing">  
           <img
                 src={ResumeMan}
                 alt="icon of a my an a resume together"
